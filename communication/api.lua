957791b080f145939761cd7b23beea48236e3e0a
imitoy <imitoy@outlook.com>
2026-03-30 15:30:12 +0800

    Format

--- communication/api.lua
+++ communication/api.lua
@@ -1,74 +1,73 @@
 local api = {}
 
-local luapython = require"luapython"
-local models = require"models"
-local avatar = require"avatar"
+local luapython = require "luapython"
+local models = require "models"
+local avatar = require "avatar"
 
 function api.select(model)
- local model_info = models[model]
-  return api[model_info.api_type]
+    local model_info = models[model]
+    return api[model_info.api_type]
 end
 
 api.openai = {}
 
 
 api.openai.defaultmessage = {
-  "system" = "You are a helpful assistant.",
+    system = "You are a helpful assistant.",
 }
 
 function api.openai.generatemessage(avatar_name)
-  if avatar_name == nil then
-    return api.openai.defaultmessage
-  end
-  for i, v in ipairs(avatar)do
-    if v.name == avatar_name then
-      return v
+    if avatar_name == nil then
+        return api.openai.defaultmessage
     end
-  end
-  return nil
+    for i, v in ipairs(avatar) do
+        if v.name == avatar_name then
+            return v
+        end
+    end
+    return nil
 end
 
 function api.openai.create(model)
-  
-  local OpenAI = luapython.impor("openai.OpenAI")
-  local client = OpenAI({
-    api_key = model.authentication.api_key,
-    base_url = model.base_url
-  })
-  return client, model
+    local OpenAI = luapython.impor("openai.OpenAI")
+    local client = OpenAI({
+        api_key = model.authentication.api_key,
+        base_url = model.base_url
+    })
+    return client, model
 end
 
 function api.openai.send(client, model, model_name, messages)
-  do
-    local find = false
-    for i, v in ipairs(model.models)do
-      if v == model_name then
-        find = true
-      end
+    do
+        local find = false
+        for i, v in ipairs(model.models) do
+            if v == model_name then
+                find = true
+            end
+        end
+        if not (find) then
+            error("api.openai,send: model_name " .. model_name .. " not found")
+        end
     end
-    if not(find) then
-      error("api.openai,send: model_name "..model_name.." not found")
+    if type(messages) ~= "table" then
+        error("api.openai.send: messages: table expected, got " .. type(messages))
     end
-  end
-  if type(messages) ~= "table" then
-    error("api.openai.send: messages: table expected, got "..type(messages))
-  end
-  local response = client.chat.completions.create({
-    model = model_name,
-    messages = messages,
-    stream = true
-  })
-  return response
+    local response = client.chat.completions.create({
+        model = model_name,
+        messages = messages,
+        stream = true
+    })
+    return response
 end
 
 function api.openai.get(response, messages)
-  local message = ""
-  for chunk in response() do
-    local stream = chunk.choices[0].delta.content
-    message = message..stream
-    coroutine.yield(false, stream)
-  end
-  return true, messages
+    local message = ""
+    for chunk in response() do
+        local stream = chunk.choices[0].delta.content
+        message = message .. stream
+        coroutine.yield(false, stream)
+    end
+    return true, messages
 end
 
 return api
--- communication/communication.lua
+++ communication/communication.lua
@@ -5,31 +5,31 @@ local communication = {}
 communication.__index = communication
 
 function communication:new(model, model_name, avatar_name)
-	local mcommunication = {}
-	setmetatable(mcommunication, self)
-	mcommunication.register = true
-	mcommunication.api = api[model.api_type]
-	mcommunication.client = mcommunication.api.create(model)
-	mcommunication.avatat_name = avatar_name
-	mcommunication.model_name = model_name
-	mcommunication.model = model
-	return mcommunication
+    local mcommunication = {}
+    setmetatable(mcommunication, self)
+    mcommunication.register = true
+    mcommunication.api = api[model.api_type]
+    mcommunication.client = mcommunication.api.create(model)
+    mcommunication.avatat_name = avatar_name
+    mcommunication.model_name = model_name
+    mcommunication.model = model
+    return mcommunication
 end
 
 function communication:send(str, user)
-	if type(self) ~= "table" or not self.register then
-		error("self: recognize failed")
-	end
-	if type(user) == nil then
-		user = "user"
-	elseif type(user) ~= "string" then
-		error("user: string expected, got " .. type(user))
-	end
-	if not self.messages then
-		self.messages = communication.api.generatemessages(self.avatar_name)
-	end
-	table.insert(self.messages)
-	self.api.send(self.client, self.model, self.model_name, self.messages)
+    if type(self) ~= "table" or not self.register then
+        error("self: recognize failed")
+    end
+    if type(user) == nil then
+        user = "user"
+    elseif type(user) ~= "string" then
+        error("user: string expected, got " .. type(user))
+    end
+    if not self.messages then
+        self.messages = communication.api.generatemessages(self.avatar_name)
+    end
+    table.insert(self.messages)
+    self.api.send(self.client, self.model, self.model_name, self.messages)
 end
 
 return communication
